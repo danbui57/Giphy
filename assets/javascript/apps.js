13b8156915d79cbe53f93845825501b96538cd8d
danbui57 <danbui57@gmail.com>
2018-12-31 16:54:34 -0500

    appends 10 gifs at a time and clears everytime a button is pressed

--- assets/javascript/apps.js
+++ assets/javascript/apps.js
@@ -3,125 +3,77 @@ var celebrities = ["Rhianna", "Kate Perry", "Donald Glover", "Jennifer Lawrence"
     "Tyra Banks", "Beyonce Knowles", "Brad Pitt", "Kanye West", "Megan Fox",
     "Johnny Depp", "Eminem", "Sofia Vergara", "Halle Barry", "Drake"];
 
-    function displayGyphy () {
+function displayGyphy() {
+    $("#images").empty();
 
-        for (var i = 0;i < celebrities.length;i ++)
+    var topic = $(this).attr("data-type");
 
-// var topic = $(this).attr("data-name");        
-
-
-
-    var topic = $(this).attr("data-name");
-
-    var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=YtZ762px9N1y2WYdOGGN1yY2onb21QLl&tag=" + topic;
-                    
-
-console.log(queryURL)
-    // " + topic;
-    
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=YtZ762px9N1y2WYdOGGN1yY2onb21QLl&limit=10";
 
     $.ajax({
         url: queryURL,
         method: "GET"
-      }).then(function(response) { 
-console.log(response)
-       
+    }).then(function (response) {
 
-           
-        var imageUrl = response.data.images.fixed_height_still.url;
-        var dataStill = response.data.images.fixed_height_still.url;
-        var dataAnimate = response.data.images.fixed_height.url;
-            
-            //
+        var res = response.data;
+        console.log(res)
+        for (var i = 0; i < res.length; i++) {
+
+            var celebsDiv = $("<span class='animal-item'>");
+
+            var imageUrl = res[i].images.fixed_height_still.url;
+            var dataAnimate = res[i].images.fixed_height.url;
+            console.log("image", imageUrl)
+
             var celebs = $("<img>");
-  
-           
-            
-            
-          
+
             celebs.attr("src", imageUrl);
-            celebs.attr("data-still", dataStill)
+            celebs.attr("data-still", imageUrl)
             celebs.attr("data-animate", dataAnimate)
             celebs.attr("data-state", "still");
             celebs.attr("class", "gif");
             celebs.attr("alt", "celebrity");
-  
-           
-            
-            //
-            $("#images").prepend(celebs);
-        
-      
-        
-    });
 
+            celebsDiv.append(celebs)
+            $("#images").append(celebsDiv);
+        }
+    });
 };
 
-$(document).on("click", ".gif", function() {
+$(document).on("click", ".gif", function () {
 
-    
-
-            
-    // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
     var state = $(this).attr("data-state");
-     console.log(state)
-    // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-    // Then, set the image's data-state to animate
-    // Else set src to the data-still value
+    console.log("state", state)
+
     if (state === "still") {
         $(this).attr("src", $(this).attr("data-animate"));
         $(this).attr("data-state", "animate");
     } else {
         $(this).attr("src", $(this).attr("data-still"));
-        $(this).attr("data-state","still");
-    
-    
+        $(this).attr("data-state", "still");
     }
 });
 
-    function renderButtons() {
+function renderButtons() {
+    $(".celebrity-buttons").empty();
+    for (var i = 0; i < celebrities.length; i++) {
 
-        $("#celebrity-buttons").empty();
-        
-
-
-        for (var i = 0; i < celebrities.length; i++) {
-
-            console.log(celebrities[i])
-
-            var a = $("<button>");
-
-            a.addClass("celebrity");
-
-            a.attr("data-name", celebrities[i] );
-
-            a.text(celebrities[i]);
-
-            $("#celebrity-buttons").append(a);
- 
-        }
+        var a = $("<button>");
+        a.addClass("celebrity");
+        a.attr("data-type", celebrities[i]);
+        a.text(celebrities[i]);
+        $(".celebrity-buttons").append(a);
     }
+}
 
-    $("#add-celebrity").on("click", function (event) {
+$("#add-celebrity").on("click", function (event) {
 
-        
+    event.preventDefault();
+    var celeb = $("input").val().trim();
+    celebrities.push(celeb);
+    renderButtons();
+});
 
-        event.preventDefault();
-       
-        var celeb = $("#celebrity-input").val().trim();
+$(document).on("click", ".celebrity", displayGyphy);
 
-        celebrities.push(celeb);
-
-        renderButtons();
-        
-        
-    });
-
-    $(document).on("click", ".celebrity", displayGyphy);
-
-
-
-    
-
-    
-      renderButtons();
+renderButtons();
